729a9f9f77d7377d84eb9c97620946d3a952b70f
Jeff <jeffreylehman94@gmail.com>
2017-12-23 22:10:45 -0500

    Added new file

--- venv/Lib/Market_Book_MarketPrice.py
+++ venv/Lib/Market_Book_MarketPrice.py
@@ -3,20 +3,25 @@ import ccxt
 
 
 # Example of depth working correctly with poloniex
-polo = 'XMR/BTC'
-exchange = ccxt.poloniex()
-markets = exchange.load_markets()
-book1 = ccxt.poloniex().fetch_order_book(polo, {'depth': 2})
+symbol = 'XMR/BTC'
+exchange1 = ccxt.poloniex()
+markets1 = exchange1.load_markets()
+book1 = ccxt.poloniex().fetch_order_book(symbol, {'depth': 2})
 print (book1)
 
 ## Example of depth not working for bittrex
-exchange = ccxt.bittrex()
-markets = exchange.load_markets()
-book = ccxt.bittrex().fetch_order_book(polo, {'depth': 2})
-print (book)
+exchange2 = ccxt.bittrex()
+markets2 = exchange2.load_markets()
+book2 = ccxt.bittrex().fetch_order_book(symbol, {'depth': 2})
+print (book2)
 
 ## Gets the market price and spread
-bid = book['bids'][0][0] if len (book['bids']) > 0 else None
-ask = book['asks'][0][0] if len (book['asks']) > 0 else None
-spread = (ask - bid) if (bid and ask) else None
-print (exchange.id, 'market price', { 'bid': bid, 'ask': ask, 'spread': spread })
+bid1 = book1['bids'][0][0] if len (book1['bids']) > 0 else None
+ask1 = book1['asks'][0][0] if len (book1['asks']) > 0 else None
+spread1 = (ask1 - bid1) if (bid1 and ask1) else None
+print (exchange1.id, 'market price', { 'bid': bid1, 'ask': ask1, 'spread': spread1 })
+
+bid2 = book2['bids'][0][0] if len (book2['bids']) > 0 else None
+ask2 = book2['asks'][0][0] if len (book2['asks']) > 0 else None
+spread2 = (ask2 - bid2) if (bid2 and ask2) else None
+print (exchange1.id, 'market price', { 'bid': bid2, 'ask': ask2, 'spread': spread2})
